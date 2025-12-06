2f6d8469e20a2c44f3f030d2ee51749d9c625cb7
whatuptkhere <R9000R@gmail.com>
2025-12-06 15:26:01 +1100

    Add files via upload

---
+++ ButtonPusher.ino
@@ -0,0 +1,182 @@
+/// @file    ButtonPusher.ino
+/// @brief   This is the device that recieves instruciton over ESP-NOW and then moves a servo to push a button.
+// The button pusher is the device that actually uses the servo to push a physical button.
+// It does this when it receives a message from the button trigger.
+// Created by TK
+
+#include <Arduino.h>
+#include <FastLED.h>
+
+#include <ESP32Servo.h>
+#include <WiFi.h>
+#include <esp_now.h>
+
+//what is this board's MAC address? Find that out.
+
+//You'll need to ID the MAC address for your own board so the trigger knows who to send to. Look up an explainer on how to do that.
+
+
+// How many leds in your strip?
+#define NUM_LEDS 1
+
+// For led chips like WS2812, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+// Clock pin only needed for SPI based chipsets when not using hardware SPI
+#define DATA_PIN 21
+#define CLOCK_PIN 13
+
+
+//Structure for ESP-NOW message. Must match sender structure.
+
+typedef struct struct_message {
+    char a[32];
+    int b;
+    float c;
+    bool d;
+} struct_message;
+
+
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+Servo myservo;  // create Servo object to control a servo
+int servoPin = 1;
+
+// Create a struct_message called myData
+struct_message myData;
+
+
+
+
+void setup() {
+  
+  //ESP32 servo init nonsense goes here. what a shitty library.
+  ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+  
+  myservo.setPeriodHertz(50); // Standard 50hz servo
+  myservo.attach(servoPin, 500, 2400);   // attaches the servo on pin 1 to the servo object
+                                         // using SG90 servo min/max of 500us and 2400us
+                                         // for MG995 large servo, use 1000us and 2000us,
+                                         // which are the defaults, so this line could be
+                                         // "myservo.attach(servoPin);"
+
+
+                           
+  // Initialize Serial Monitor
+  Serial.begin(115200);
+  
+  // Set device as a Wi-Fi Station
+  WiFi.mode(WIFI_STA);
+
+  // Init ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+
+  
+  //Serial.begin(9600);
+  //Serial.println("BLINK setup starting");
+
+  // Uncomment/edit one of the following lines for your leds arrangement.
+  // ## Clockless types ##
+  //FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+
+  //Serial.println("BLINK setup complete");
+  // FastLED.addLeds<SM16824E, DATA_PIN, RGB>(leds, NUM_LEDS);  // RGB ordering (uses SM16824EController)
+  // FastLED.addLeds<SM16703, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1829, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1812, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1809, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1804, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<TM1803, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<UCS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<UCS1903B, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<UCS1904, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<UCS2903, DATA_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+  // FastLED.addLeds<WS2852, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+  // FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+  // FastLED.addLeds<GS1903, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<SK6812, DATA_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+  // FastLED.addLeds<SK6822, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<APA106, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<PL9823, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<SK6822, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2813, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<APA104, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2811_400, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<GE8822, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<GW6205, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<GW6205_400, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<LPD1886, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<LPD1886_8BIT, DATA_PIN, RGB>(leds, NUM_LEDS);
+  // ## Clocked (SPI) types ##
+  // FastLED.addLeds<LPD6803, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+  // FastLED.addLeds<LPD8806, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);  // GRB ordering is typical
+  // FastLED.addLeds<WS2801, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<WS2803, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<SM16716, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+  // FastLED.addLeds<P9813, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);  // BGR ordering is typical
+  // FastLED.addLeds<DOTSTAR, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);  // BGR ordering is typical
+  // FastLED.addLeds<APA102, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);  // BGR ordering is typical
+  // FastLED.addLeds<SK9822, DATA_PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);  // BGR ordering is typical
+
+
+
+}
+
+void loop() {
+  //Serial.println("BLINK");
+  
+
+
+  
+
+  // Turn the LED on, then pause
+  leds[0] = CRGB::Aqua;
+  FastLED.show();
+  delay(1500);
+
+  // Now turn the LED off, then pause
+  leds[0] = CRGB::Turquoise;
+  FastLED.show();
+  delay(100);
+
+  //myservo.write(55);
+  delay(2000);
+}
+
+
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  Serial.print("Bytes received: ");
+  Serial.println(len);
+  Serial.print("Char: ");
+  Serial.println(myData.a);
+  Serial.print("Int: ");
+  Serial.println(myData.b);
+  Serial.print("Float: ");
+  Serial.println(myData.c);
+  Serial.print("Bool: ");
+  Serial.println(myData.d);
+  Serial.println();
+  
+  //move servo
+  myservo.write(20);
+  //wait
+  delay(500);
+  //move servo again
+  myservo.write(0);
+
+}
